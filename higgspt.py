50e62de420a5e871917228336cb56d0a1f3a83b9
keshavrmenon <keshavmenon@gmail.com>
2020-02-27 01:14:16 +0530

    modified:   higgspt.py

--- higgspt.py
+++ higgspt.py
@@ -3,16 +3,16 @@ from ROOT import TFile, TCanvas, TH1F, TLegend, TLatex, TLorentzVector
 
 c=TCanvas()
 
-data1=readLHEF('../LHE/NMSSM/m300.lhe')
+data1=readLHEF('../LHE/spin0/m300.lhe')
 events1=data1.events
-data2=readLHEF('../LHE/NMSSM/m600.lhe')
+data2=readLHEF('../LHE/spin0/m600.lhe')
 events2=data2.events
-data3=readLHEF('../LHE/NMSSM/m1000.lhe')
+data3=readLHEF('../LHE/spin0/m900.lhe')
 events3=data3.events
 
 hpt_h_300=TH1F("pt_h_300", "M_{X}=300", 100, 0, 500)
 hpt_h_600=TH1F("pt_h_600", "M_{X}=600", 100, 0, 500)
-hpt_h_1000=TH1F("pt_h_1000", "M_{X}=1000", 100, 0, 500)
+hpt_h_1000=TH1F("pt_h_1000", "M_{X}=900", 100, 0, 500)
 
 for e in events1:
     higgs=e.getParticlesByIDs([25])
@@ -42,5 +42,5 @@ hpt_h_300.Draw("hist")
 hpt_h_600.Draw("hist&same")
 hpt_h_1000.Draw("hist&same")
 l=c.BuildLegend(0.75, 0.4, 0.9, 0.5)
-hpt_h_300.SetTitle("p_{T} [in GeV] of Higgs in NMSSM for different masses of X")
-c.SaveAs("./comparison/h_pt_compare.root")
+hpt_h_300.SetTitle("p_{T} [in GeV] of Higgs in Radion Model for different masses of X")
+c.SaveAs("./comparison/h_pt_compare_radion.root")
